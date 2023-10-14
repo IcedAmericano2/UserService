5188375d32b1f92949f4665ef8ef54c4e332810d
RyuChaeHyun <lch010201@gmail.com>
2023-10-14 14:10:55 +0900

    fix: Cors 설정 수정 #2

---
+++ src/main/java/com/example/UserService/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.example.UserService.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(List.of("http://13.124.68.5"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setExposedHeaders(List.of("*"));
+
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
+}
--- src/main/java/com/example/UserService/controller/UserController.java
+++ src/main/java/com/example/UserService/controller/UserController.java
@@ -33,14 +33,12 @@ public class UserController {
         return "redirect:http://localhost:3000";
     }
 
-    @CrossOrigin(origins = "/**")
     @PostMapping("/login")
     public ResponseEntity<JWTAuthResponse> login(@RequestBody RequestLogin requestLogin){
         JWTAuthResponse token = userService.login(requestLogin);
         return ResponseEntity.ok(token);
     }
 
-    @CrossOrigin(origins = "/**")
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody RequestUser requestUser){
         String response = userService.register(requestUser);

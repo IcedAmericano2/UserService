48d7a87327bf59d83c819890493bfa8e355b57ca
RyuChaeHyun <lch010201@gmail.com>
2023-10-15 14:50:25 +0900

    fix: Cors 설정 허용 - options 메소드 처리

--- src/main/java/com/example/UserService/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/UserService/config/JwtAuthenticationFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
@@ -22,17 +23,22 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
 
-        // get JWT token from http request
-        String token = jwtTokenProvider.resolveToken(request);
-
-        // validate token
-        if (StringUtils.hasText(token) && jwtTokenProvider.validateToken(token)) {
-
-            Authentication authentication = jwtTokenProvider.getAuthentication(token);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-
+        if (HttpMethod.OPTIONS.matches(request.getMethod())) {
+            // OPTIONS 요청에 대한 처리 (예: CORS 관련 헤더 설정)
+            // 토큰이 필요 없으므로 여기에서 토큰 관련 검사를 수행하지 않음
         }
+        else{
+            // get JWT token from http request
+            String token = jwtTokenProvider.resolveToken(request);
 
+            // validate token
+            if (StringUtils.hasText(token) && jwtTokenProvider.validateToken(token)) {
+
+                Authentication authentication = jwtTokenProvider.getAuthentication(token);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            }
+        }
         filterChain.doFilter(request, response);
     }
 

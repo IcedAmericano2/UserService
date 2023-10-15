3f1c4cdcc779025afdaab3d42520f82fe624a1c5
RyuChaeHyun <lch010201@gmail.com>
2023-10-15 12:04:26 +0900

    fix: Cors 설정 수정 - Options 허용

--- src/main/java/com/example/UserService/config/CorsConfig.java
+++ src/main/java/com/example/UserService/config/CorsConfig.java
@@ -19,10 +19,11 @@ public class CorsConfig {
         config.setAllowCredentials(true);
         config.addAllowedOriginPattern("http://15.164.100.22:8081"); //특정 패턴 Origin만 허용
         config.addAllowedOriginPattern("http://13.124.68.5:3000");
+        config.addAllowedOriginPattern("http://localhost:3000");
 //        config.setAllowedOrigins(List.of("http://localhost:3000","http://13.124.68.5"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
-        config.setExposedHeaders(List.of("Authorization"));
+        config.setExposedHeaders(List.of("Authorization", "jwt-token"));
         source.registerCorsConfiguration("/**", config);
 
         return new CorsFilter(source);
--- src/main/java/com/example/UserService/config/SecurityConfig.java
+++ src/main/java/com/example/UserService/config/SecurityConfig.java
@@ -4,7 +4,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,6 +15,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -48,7 +55,7 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers(WHITE_LIST).permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, WHITE_LIST).permitAll()
                         .anyRequest().authenticated() //어떠한 요청이라도 인증 필요
                 )
                 .exceptionHandling(exception -> exception

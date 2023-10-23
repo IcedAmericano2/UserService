462e82511a7a95fd374d2ca7fa1376b2d1682e1b
RyuChaeHyun <lch010201@gmail.com>
2023-10-23 22:37:56 +0900

    build: jar 파일 업데이트

--- src/main/java/com/example/UserService/UserServiceApplication.java
+++ src/main/java/com/example/UserService/UserServiceApplication.java
@@ -2,11 +2,10 @@ package com.example.UserService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class UserServiceApplication {
 
